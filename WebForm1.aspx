934e9c31d861475c1e8c8bcabfd4e3cc18260730
srmiot <uzzodot@gmail.com>
2024-10-21 00:19:23 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Dynamic_GriedView.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" style="z-index: 1; left: 568px; top: 297px; position: absolute; height: 152px; width: 232px">
+            </asp:GridView>
+        </div>
+        <asp:TextBox ID="TextBox1" runat="server" style="z-index: 1; left: 620px; top: 94px; position: absolute"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server" style="z-index: 1; left: 621px; top: 158px; position: absolute"></asp:TextBox>
+        <asp:Label ID="Label1" runat="server" style="position: absolute; z-index: 1; left: 519px; top: 95px" Text="Name"></asp:Label>
+        <p>
+            <asp:Label ID="Label2" runat="server" style="z-index: 1; left: 517px; top: 163px; position: absolute" Text="Village"></asp:Label>
+        </p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="z-index: 1; left: 659px; top: 229px; position: absolute" Text="Submit" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Dynamic_GriedView
+{
+    
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection c = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\Sem 5\AWP\Dynamic_GriedView\App_Data\Database1.mdf"";Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            c.Open();
+            SqlCommand cmd2= new SqlCommand("select Name,Village from SD",c);
+            GridView1.DataSource = cmd2.ExecuteReader();
+            GridView1.DataBind();
+            c.Close();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //SqlConnection c = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\Sem 5\AWP\Dynamic_GriedView\App_Data\Database1.mdf"";Integrated Security=True");
+            c.Open();
+            SqlCommand cmd = new SqlCommand("insert into SD values('"+TextBox1.Text+"','"+TextBox2.Text+"')",c);
+            cmd.ExecuteNonQuery();
+            c.Close();
+
+            //SqlConnection c3 = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\Sem 5\AWP\Dynamic_GriedView\App_Data\Database1.mdf"";Integrated Security=True");
+            c.Open();
+            SqlCommand cmd3 = new SqlCommand("Select Name,Village from SD",c);
+            GridView1.DataSource=cmd3.ExecuteReader();
+            GridView1.DataBind();
+            c.Close();
+        }
+    }
+}
